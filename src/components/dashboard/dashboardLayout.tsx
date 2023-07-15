8e889e7d8de7ea2813433b27f936765400e58600
Sahid Rahman <sahidrahman404@gmail.com>
2023-07-15 10:56:48 +0700

    add dashboard layout

---
+++ src/components/dashboard/dashboardLayout.tsx
@@ -0,0 +1,152 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { ReactNode, useState } from "react";
+
+export function DashboardLayout({ children }: { children: ReactNode }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isDashboardFocus, setIsDashboardFocus] = useState(false);
+  const [isAccountFocus, setIsAccountFocus] = useState(false);
+
+  const currentRoute = useRouter().asPath;
+
+  return (
+    <div>
+      <div className="sticky top-0 inset-x-0 z-20 bg-white border-y px-4 sm:px-6 md:px-8 lg:hidden dark:bg-gray-800 dark:border-gray-700">
+        <div
+          className="flex items-center py-4"
+          onClick={() => {
+            setIsOpen((val) => {
+              if (val) {
+                return false;
+              }
+              return val;
+            });
+          }}
+        >
+          <button
+            type="button"
+            className="text-gray-500 hover:text-gray-600"
+            data-hs-overlay="#application-sidebar"
+            aria-controls="application-sidebar"
+            aria-label="Toggle navigation"
+            onClick={(event) => {
+              event.stopPropagation();
+              setIsOpen((val) => !val);
+            }}
+          >
+            <span className="sr-only">Toggle Navigation</span>
+            <svg
+              className="w-5 h-5"
+              width="16"
+              height="16"
+              fill="currentColor"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fillRule="evenodd"
+                d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+
+      <div
+        id="application-sidebar"
+        className={
+          isOpen
+            ? "translate-x-0 transition-all duration-300 transform block fixed top-0 left-0 bottom-0 z-[60] w-64 bg-white border-r border-gray-200 pt-7 pb-10 overflow-y-auto scrollbar-y lg:block lg:translate-x-0 lg:right-auto lg:bottom-0 dark:scrollbar-y dark:bg-gray-800 dark:border-gray-700"
+            : "-translate-x-full transition-all duration-300 transform hidden fixed top-0 left-0 bottom-0 z-[60] w-64 bg-white border-r border-gray-200 pt-7 pb-10 overflow-y-auto scrollbar-y lg:block lg:translate-x-0 lg:right-auto lg:bottom-0 dark:scrollbar-y dark:bg-gray-800 dark:border-gray-700"
+        }
+      >
+        <div className="px-6">
+          <Link
+            className="flex-none text-xl font-semibold dark:text-white"
+            href="/"
+            aria-label="Brand"
+          >
+            Gigachad
+          </Link>
+        </div>
+
+        <nav
+          className="hs-accordion-group p-6 w-full flex flex-col flex-wrap"
+          data-hs-accordion-always-open
+        >
+          <ul className="space-y-1.5">
+            <li>
+              <Link
+                className={
+                  currentRoute.match(/\/dashboard/) !== null
+                    ? "bg-gray-100 flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-slate-700 rounded-md hover:bg-gray-100 dark:bg-gray-900 dark:text-white"
+                    : "flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-slate-700 rounded-md hover:bg-gray-100 dark:bg-gray-900 dark:text-white"
+                }
+                href="/dashboard"
+              >
+                <svg
+                  className="w-3.5 h-3.5"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"
+                  />
+                  <path
+                    fillRule="evenodd"
+                    d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"
+                  />
+                </svg>
+                Dashboard
+              </Link>
+            </li>
+
+            <li className="hs-accordion" id="account-accordion">
+              <Link
+                className={
+                  currentRoute.match(/\/account/) !== null
+                    ? "bg-gray-100 flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-slate-700 rounded-md hover:bg-gray-100 dark:bg-gray-900 dark:text-white"
+                    : "flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-slate-700 rounded-md hover:bg-gray-100 dark:bg-gray-900 dark:text-white"
+                }
+                href="/account"
+              >
+                <svg
+                  className="w-3.5 h-3.5"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                  <path
+                    fillRule="evenodd"
+                    d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
+                  />
+                </svg>
+                Account
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <div
+        className="w-full pt-10 px-4 sm:px-6 md:px-8 lg:pl-72"
+        onClick={() => {
+          setIsOpen((val) => {
+            if (val) {
+              return false;
+            }
+            return val;
+          });
+        }}
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
