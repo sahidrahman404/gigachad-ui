f757bc9795197073219a87f60c81ea7cdd495fd6
Sahid Rahman <sahidrahman404@gmail.com>
2023-07-16 12:31:55 +0700

    update dashboard component

--- src/components/dashboard/DashboardComponent.tsx
+++ src/components/dashboard/DashboardComponent.tsx
@@ -1,8 +1,10 @@
 import GetUserQuery from "@/gql/GetUser";
 import { useRouter } from "next/router";
-import { useMemo, useEffect } from "react";
-import { PreloadedQuery, usePreloadedQuery } from "react-relay";
+import { useMemo, useEffect, Suspense } from "react";
+import { PreloadedQuery, usePreloadedQuery, useQueryLoader } from "react-relay";
 import { GetUserQuery as GetUserQueryType } from "../../../__generated__/GetUserQuery.graphql";
+import Routines, { routinesQuery } from "./Routines";
+import { RoutinesQuery } from "../../../__generated__/RoutinesQuery.graphql";
 
 export type DashboardProps = {
   getUserQueryRef: PreloadedQuery<GetUserQueryType, Record<string, unknown>>;
@@ -15,6 +17,10 @@ export default function DashboardComponent({
     GetUserQuery,
     getUserQueryRef
   );
+
+  const [routineQueryRef, loadRoutineQuery] =
+    useQueryLoader<RoutinesQuery>(routinesQuery);
+
   const router = useRouter();
 
   const userMemo = useMemo(() => user, [user.getUser?.id]);
@@ -23,7 +29,23 @@ export default function DashboardComponent({
     if (user.getUser?.activated === 0 || user.getUser === null) {
       router.push("/auth?mode=signin");
     }
+
+    if (user.getUser && user.getUser.id) {
+      loadRoutineQuery({ input: user.getUser.id });
+      return;
+    }
+
+    loadRoutineQuery({ input: "" });
   }, [userMemo]);
 
-  return <p>dashboard</p>;
+  return (
+    <Suspense fallback="loading...">
+      {routineQueryRef && (
+        <div className="space-y-4">
+          <p className="text-2xl font-bold">Routines</p>
+          <Routines routinesQueryRef={routineQueryRef} />
+        </div>
+      )}
+    </Suspense>
+  );
 }
